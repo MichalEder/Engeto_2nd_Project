cd594e34959de693c1033897e0efe5b2aecef6aa
MichalEder <edermichal.eder@gmail.com>
2023-05-01 11:03:24 +0200

    zakladni struktura

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+2nd1st_Project_ENGETO: druhý projekt do Engeto Online Python Akademie
+author: Michal Eder
+email: edermichal.eder@gmail.com
+discord: Michal Eder#2018
+'''
+
+# importy
+
+# hlavni fce
+    #pozdrav
+
+# def pozdrav
+
+# def herni pole
+
+# def ziskej tah hrace
+
+# ziskej tah AI
+    #minimax
+
+# kontrola stavu hry
+
